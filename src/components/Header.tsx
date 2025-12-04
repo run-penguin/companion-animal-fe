d2a5435ff1d91777522986ca520c82702f6304c1
runpenguin <run-penguin@naver.com>
2025-12-04 16:23:37 +0900

    각 페이지마다 Header를 Custom하게 적용하기 쉽도록 기존 Header 컴포넌트 삭제

--- src/components/Header.tsx
+++
@@ -1,7 +0,0 @@
-import "./Header.css";
-
-const Header = () => {
-  return <div className="header">분실동물 조회</div>;
-};
-
-export default Header;
--- src/pages/loss/Home.tsx
+++ src/pages/loss/Home.tsx
@@ -1,10 +1,29 @@
 import SearchBar from "../../components/loss/search/SearchBar";
 import LossList from "../../components/loss/list/LossList";
 import NavigationBar from "../../components/loss/pagination/Pagination";
+import Button from "../../components/Button";
+import { BUTTON_TYPE } from "../../util/constants";
+import { useNavigate } from "react-router-dom";
+import "./Home.css";
 
 export default function Home() {
+  const navigate = useNavigate();
+
+  const onClickMap = () => {
+    navigate("/map");
+  };
+
   return (
     <>
+      <div className="header">
+        <div></div>
+        <label>분실동물 조회</label>
+        <Button
+          type={BUTTON_TYPE.MAP.value}
+          isText={true}
+          onClick={onClickMap}
+        />
+      </div>
       <SearchBar />
       <NavigationBar />
       <LossList />
